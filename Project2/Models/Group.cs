8ec5a50549fa92cefddd084278f2ed3edb9ccbf3
Kyle Evert <kevert14@byu.edu>
2022-03-21 10:06:27 -0600

    input model data

--- Project2/Models/Group.cs
+++ Project2/Models/Group.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,20 @@ namespace Project2.Models
 {
     public class Group
     {
+        [Key]
+        [Required]
+        public long BookId { get; set; }
+        [Required]
+        public string GroupName { get; set; }
+        [Required]
+        [Range(0, 15)]
+        public string GroupSize { get; set; }
+        [Required]
+        [EmailAddress]
+        public string ContactEmail { get; set; }
+        public string Phone { get; set; }
+
+
+
     }
 }
--- Project2/Models/TimeSlot.cs
+++ Project2/Models/TimeSlot.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,15 @@ namespace Project2.Models
 {
     public class TimeSlot
     {
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public DateTime Time { get; set; }
+        [Required]
+        public Boolean Available { get; set; }
+        [Key]
+        [Required]
+        public long GroupId { get; set; }
+
     }
 }
